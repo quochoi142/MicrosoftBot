093cd26dd85c909f09f6f14f1586392dfa237a67
Pedemanggiaydo <dada8028@gmail.com>
2020-04-16 16:04:13 +0700

    test channel data

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -115,37 +115,103 @@ class MainDialog extends ComponentDialog {
                 return await stepContext.beginDialog('routeDialog', routeDetails);
             }
             case 'Tìm_xe_bus': {
+                /*  await stepContext.context.sendActivity({
+                     text: "test",
+                     channelData: {
+                         "attachment": {
+                             "type": "template",
+                             "payload": {
+                                 "template_type": "button",
+                                 "text": "What do you want to do next?",
+                                 "buttons": [
+                                     {
+                                         "type": "postback",
+                                         "title": "View More 1",
+                                         "payload": "View More"
+                                     },
+                                     {
+                                         "type": "postback",
+                                         "title": "View More 2",
+                                         "payload": "View More"
+                                     },
+                                     {
+                                         "type": "postback",
+                                         "title": "View More 3",
+                                         "payload": "View More"
+                                     }
+                                 ]
+                             }
+                         }
+                     }
+                 }); */
                 await stepContext.context.sendActivity({
                     text: "test",
                     channelData: {
                         "attachment": {
                             "type": "template",
                             "payload": {
-                                "template_type": "button",
-                                "text": "What do you want to do next?",
+                                "template_type": "list",
+                                "top_element_style": "compact",
+                                "elements": [
+                                    {
+                                        "title": "Classic T-Shirt Collection",
+                                        "subtitle": "See all our colors",
+                                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                                        "buttons": [
+                                            {
+                                                "title": "View",
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                                                "messenger_extensions": true,
+                                                "webview_height_ratio": "tall",
+                                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        "title": "Classic White T-Shirt",
+                                        "subtitle": "See all our colors",
+                                        "default_action": {
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                                            "messenger_extensions": false,
+                                            "webview_height_ratio": "tall"
+                                        }
+                                    },
+                                    {
+                                        "title": "Classic Blue T-Shirt",
+                                        "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                                        "subtitle": "100% Cotton, 200% Comfortable",
+                                        "default_action": {
+                                            "type": "web_url",
+                                            "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                                            "messenger_extensions": true,
+                                            "webview_height_ratio": "tall",
+                                            "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                        },
+                                        "buttons": [
+                                            {
+                                                "title": "Shop Now",
+                                                "type": "web_url",
+                                                "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                                                "messenger_extensions": true,
+                                                "webview_height_ratio": "tall",
+                                                "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                                            }
+                                        ]
+                                    }
+                                ],
                                 "buttons": [
                                     {
+                                        "title": "View More",
                                         "type": "postback",
-                                        "title": "View More 1",
-                                        "payload": "View More"
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "View More 2",
-                                        "payload": "View More"
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "View More 3",
-                                        "payload": "View More"
+                                        "payload": "payload"
                                     }
                                 ]
                             }
                         }
                     }
-                }
-                );
-
+                });
                 //chỉ hiện location card
                 return await stepContext.beginDialog('searchDialog', routeDetails);
             }
